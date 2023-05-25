da6a590e058bd48f06dbd11e09fae113a11c97e8
nala <zarahah22@gmail.com>
2023-05-25 12:59:43 +0700

    task 23

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use App\Http\Controllers\CRUDController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/landing', [LandingController::class, 'index'])->name('landing');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
+Route::get('/role', [RoleController::class, 'index'])->name('role.index');
 Route::get('/', function () {
     return view('welcome');
 });
